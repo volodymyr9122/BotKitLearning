bb777da909860b7c914ccd9c9bd4110e43aa5f26
Vova M <>
2018-10-15 17:04:08 +0300

    added return in referal share

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -42,9 +42,9 @@ module.exports = function(controller, bot) {
     webserver.use(express.static('public'));
 
 
-    webserver.listen(process.env.PORT || 3000, null, function() {
+    webserver.listen(process.env.PORT || 8000, null, function() {
 
-        debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+        debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 8000);
 
     });
 
--- partials/answer.js
+++ partials/answer.js
@@ -28,57 +28,61 @@ exports.main_menu = {
      ]
 }
 
-exports.shareInviteFriend = (ref) =>({
-        "type": "template",
-        "payload": {
-            "template_type": "generic",
-            "elements": [
-                {
-                    "title": "BotKit2 best shop you ever visited",
-                    "subtitle": "Best products for low prices.",
-                    "image_url": "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/p200x200/42229935_313826186086023_3070061778734940160_n.png?_nc_cat=106&oh=aec985adc9c85f51d11272cb7632f3c4&oe=5C610DCA",
-                    "buttons": [
-                        {
-                            "type": "element_share",
-                            "share_contents": {
-                                "attachment": {
-                                    "type": "template",
-                                    "payload": {
-                                        "template_type": "generic",
-                                        "elements": [
-                                            {
-                                                "title": "It worth visiting",
-                                                "subtitle": "Hey buddy you should see it",
-                                                "image_url": "https://bot.peters-hats.com/img/hats/fez.jpg",
-                                                "default_action": {
+exports.shareInviteFriend = (ref) => ({
+    "type": "template",
+    "payload": {
+        "template_type": "generic",
+        "elements": [
+            {
+                "title": `Your link for friends is ${ref}`,
+                "subtitle": "Send link to 3 friends and get 1 product for free",
+                "image_url": "https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-1/p200x200/42229935_313826186086023_3070061778734940160_n.png?_nc_cat=106&oh=aec985adc9c85f51d11272cb7632f3c4&oe=5C610DCA",
+                "buttons": [
+                    {
+                        "type": "element_share",
+                        "share_contents": {
+                            "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "generic",
+                                    "elements": [
+                                        {
+                                            "title": "BotKit2 best shop you ever visited",
+                                            "subtitle": "Best products for low prices",
+                                            "image_url": "https://bot.peters-hats.com/img/hats/fez.jpg",
+                                            "default_action": {
+                                                "type": "web_url",
+                                                "url": `http://m.me/313826116086030?ref=${ref}`
+                                            },
+                                            "buttons": [
+                                                {
                                                     "type": "web_url",
-                                                    "url": `http://m.me/313826116086030?ref=${ref}`
-                                                },
-                                                "buttons": [
-                                                    {
-                                                        "type": "web_url",
-                                                        "url": `http://m.me/313826116086030?ref=${ref}`,
-                                                        "title": `join us your ref is ${ref}`
+                                                    "url": `http://m.me/313826116086030?ref=${ref}`,
+                                                    "title": `join us your ref is ${ref}`
                             }
                           ]
                         }
                       ]
-                                    }
                                 }
                             }
+                        }
               }
             ]
-          }
-        ]
+          },
+
+            {
+                "title": "Return",
+                "subtitle": "Return to main menu",
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Main menu",
+                        "payload": "sample_get_started_payload"
+            }
+          ]
         }
+
+        ]
+    }
 })
-
-exports.welcomeMessage = (id, bot) =>({
-
-    /* bot.say({
-                text: 'Look up and enjoy.More',
-                channel: id // a valid facebook user id or phone number
-            })*/
-})
-
 
