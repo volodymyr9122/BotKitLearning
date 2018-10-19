1a3b870991c66e3f2b0c49202a1d64e2210e7486
Vova M <>
2018-10-20 01:17:26 +0300

    heroku17

--- components/thread_settings.js
+++ components/thread_settings.js
@@ -5,13 +5,13 @@ var debug = require('debug')('botkit:thread_settings');
 module.exports = function(controller) {
 
     debug('Configuring Facebook thread settings...');
-    controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
-    controller.api.thread_settings.get_started('sample_get_started_payload');
-    controller.api.thread_settings.menu([
+    //controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
+    //controller.api.thread_settings.get_started('sample_get_started_payload');
+/*    controller.api.thread_settings.menu([
         {
             "locale": "default",
             "composer_input_disabled": false,
-            "call_to_actions": [    
+            "call_to_actions": [
                 {
                     "type":"postback",
                     "title":"Hello",
@@ -37,10 +37,10 @@ module.exports = function(controller) {
                         "title": "Main Readme",
                         "url":"https://github.com/howdyai/botkit/blob/master/readme.md",
                         "webview_height_ratio":"full",
-                      }                    
+                      }
 
                   ]
                 }
             ]
-        }]);
+        }]);*/
 }
